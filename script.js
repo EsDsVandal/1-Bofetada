57f506cc123c3999a6b144a26d42db6bee4d7a35
EsDsVandal <jesusggg1634@gmail.com>
2024-07-25 14:33:37 +0200

    ErCucurella

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", function() {
         const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
         countdownElement.innerHTML = `
-            ${days} d ${hours} h ${minutes} m ${seconds} s
+            ${days} : ${hours} : ${minutes} : ${seconds}
         `;
     }
 
